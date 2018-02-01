e3c50de9a39d5b04b227a424db8a961d92c3f9e2
Oleh Zayats <ozayats@gmail.com>
2018-02-01 14:42:36 +0200

    added additional example to DI playground

--- Sources/DependencyInjection/DependencyInjection.playground/Contents.swift
+++ Sources/DependencyInjection/DependencyInjection.playground/Contents.swift
@@ -48,3 +48,29 @@ final class StoreCoordinator {
 let httpClient = NetworkClient()
 let storeCoordinator = StoreCoordinator()
 storeCoordinator.httpClient = httpClient
+
+/* Additional case: DI with ViewControllers */
+
+// Someething we want to inject:
+struct Data { }
+
+// Generally the Property Injection is not a good idea for viewControllers
+// viewController becomes a mess, hard to test and maintain. So we can use an initializer instead:
+
+class ViewController: UIViewController {
+
+    private let data: Data
+    
+    init(with data: Data) {
+        self.data = data
+        super.init(nibName: "ViewController", bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) is not supported")
+    }
+}
+
+let data = Data()
+let viewController = ViewController(with: data)
+
